043b37365769b17c58f26dc1168dececfa08ecb4
StratiKool <skoulour@iu.edu>
2024-02-26 09:05:48 -0500

    Converted RemoteLoader class into a singleton class

--- src/main/java/edu/iu/habahram/remotecontroller/repository/RemoteLoader.java
+++ src/main/java/edu/iu/habahram/remotecontroller/repository/RemoteLoader.java
@@ -4,12 +4,26 @@ import edu.iu.habahram.remotecontroller.model.DeviceData;
 import edu.iu.habahram.remotecontroller.model.Light;
 import edu.iu.habahram.remotecontroller.model.RemoteControl;
 
+import java.rmi.Remote;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class RemoteLoader implements  IRemoteLoader{
     HashMap<Integer, RemoteControl> remoteControls = new HashMap<>();
+
+    private volatile static RemoteLoader uniqueInstance;
+
+    public static RemoteLoader getInstance(){
+        if(uniqueInstance == null){
+            synchronized (RemoteLoader.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new RemoteLoader();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
     @Override
     public void setup(int id, List<DeviceData> devices) {
         RemoteControl remoteControl = new RemoteControl(devices.size());
